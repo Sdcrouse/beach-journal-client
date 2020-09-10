51f0bf5b7c64368f35c17ac3060d40fa8603df9a
sdcro <sdcrouse91@gmail.com>
2020-09-10 16:50:05 -0700

    Move beachesReducer into separate file and directory

--- src/index.js
+++ src/index.js
@@ -6,8 +6,7 @@ import * as serviceWorker from './serviceWorker';
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
-
-const beachesReducer = () => [];
+import beachesReducer from './reducers/beachesReducer';
 
 const reducer = combineReducers({beaches: beachesReducer});
 
---
+++ src/reducers/beachesReducer.js
@@ -0,0 +1,10 @@
+import { bindActionCreators } from "redux";
+
+const beachesReducer = (state = [], action) => {
+  switch(action.type) {
+    default:
+      return state;
+  }
+};
+
+export default beachesReducer;
