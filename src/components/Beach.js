b991e59f1bbec3178f7fb5e027e0fa836053845f
sdcro <sdcrouse91@gmail.com>
2020-09-11 22:00:50 -0700

    Add Attraction information to Beach component

--- src/components/Beach.js
+++ src/components/Beach.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
 const Beach = props => {
-  const { name, description, items_to_bring, popular_activities, location } = props;
+  const {
+    name,
+    description,
+    items_to_bring,
+    popular_activities,
+    location,
+    attractions
+  } = props;
 
   return (
     <>
@@ -9,9 +16,17 @@ const Beach = props => {
       <p>
         <strong>Location: </strong>{location.city}, {location.state}, {location.country}
       </p>
+
       <p><strong>Description: </strong>{description}</p>
       <p><strong>Items to Bring: </strong>{items_to_bring}</p>
       <p><strong>Popular Activities: </strong>{popular_activities}</p>
+      
+      <h4>Attractions:</h4>
+      {attractions.map(attraction => 
+        <p key={attraction.id}>
+          <strong>{attraction.name}: </strong>{attraction.description}
+        </p>
+      )}
     </>
   )
 };
