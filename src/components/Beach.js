0e66d81c8d8f0771eab184b0daa82a59a1e6dbb1
sdcro <sdcrouse91@gmail.com>
2020-09-11 21:44:21 -0700

    Display Beach location information

--- src/components/Beach.js
+++ src/components/Beach.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
 const Beach = props => {
-  const { name, description, items_to_bring, popular_activities } = props;
+  const { name, description, items_to_bring, popular_activities, location } = props;
 
   return (
     <>
       <h3>{name}</h3>
+      <p>
+        <strong>Location: </strong>{location.city}, {location.state}, {location.country}
+      </p>
       <p><strong>Description: </strong>{description}</p>
       <p><strong>Items to Bring: </strong>{items_to_bring}</p>
       <p><strong>Popular Activities: </strong>{popular_activities}</p>
