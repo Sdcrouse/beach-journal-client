274009b1dc4a7b2c27d2f14e282c1cdef151f397
sdcro <sdcrouse91@gmail.com>
2020-09-14 20:52:53 -0700

    Add styles to navigation links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
+const linkStyles = {
+  width: '100px',
+  padding: '12px',
+  margin: '0 6px 6px',
+  background: 'blue',
+  textDecoration: 'none',
+  color: 'white'
+};
+
+const activeStyles = { background: 'darkblue' };
+
 const Navbar = () => (
   <nav>
-    <NavLink exact to="/">Home</NavLink>
-    <NavLink exact to="/beaches">Your Saved Beaches</NavLink>
+    <NavLink
+      exact
+      to="/"
+      style={linkStyles}
+      activeStyle={activeStyles}
+    >Home</NavLink>
+    
+    <NavLink
+      exact
+      to="/beaches"
+      style={linkStyles}
+      activeStyle={activeStyles}
+    >Your Saved Beaches</NavLink>
   </nav>
 );
 
