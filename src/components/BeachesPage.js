fce38cf99badcd5efe8f7bef80aecabba76846d4
sdcro <sdcrouse91@gmail.com>
2020-09-14 21:06:24 -0700

    For consistency with other components, add <header> element

--- src/components/BeachesPage.js
+++ src/components/BeachesPage.js
@@ -3,7 +3,10 @@ import Beach from './Beach';
 
 const BeachesPage = props => (
   <>
-    <h2>Your Saved Beaches:</h2>
+    <header className="App-header">
+      <h2>Your Saved Beaches:</h2>
+    </header>
+    
     {
       props.beaches.map(
         beach => <Beach key={beach.id} {...beach.attributes} />
