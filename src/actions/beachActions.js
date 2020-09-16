b2a530798fc0a95409571146c45bdb9db57af400
sdcro <sdcrouse91@gmail.com>
2020-09-16 00:11:59 -0700

    Use the Normalizr package to normalize the beach JSON
    This should ultimately make it easier to CRUD data from/in the Redux store.
    However, some of the React components will need to be updated in the next few commits.

--- src/actions/beachActions.js
+++ src/actions/beachActions.js
@@ -1,9 +1,35 @@
+import { normalize, schema } from 'normalizr';
+
 export const fetchBeaches = () => {
   return dispatch => {
     fetch("http://localhost:3000/api/v1/beaches")
       .then(resp => resp.json())
-      .then(
-        beachJson => dispatch({ type: 'LOAD_BEACHES', beaches: beachJson.data })
-      )
+      .then(beachJson => {
+        const beaches = normalizeBeachData(beachJson.data);
+
+        dispatch({ type: 'LOAD_BEACHES', beaches: beachJson.data });
+      })
   };
+};
+
+// Helper methods:
+
+const normalizeBeachData = beachData => {
+  const data = beachData.map(beach => ({
+    id: beach.id,
+    type: beach.type,
+    ...beach.attributes    
+  }));
+
+  const locationSchema = new schema.Entity('locations');
+  const attractionSchema = new schema.Entity('attractions');
+  const journalEntrySchema = new schema.Entity('journal_entries');
+  
+  const beachSchema = new schema.Entity('beaches', {
+    location: locationSchema,
+    attractions: [attractionSchema],
+    journal_entries: [journalEntrySchema]
+  });
+  
+  return normalize(data, [beachSchema]);
 }
