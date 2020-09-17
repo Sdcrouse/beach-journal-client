f28a6162ff0c6cca5eb18a4d74a7cb9608ea982a
sdcro <sdcrouse91@gmail.com>
2020-09-16 23:59:47 -0700

    Convert BeachesPage into a functional component

--- src/components/BeachesPage.js
+++ src/components/BeachesPage.js
@@ -1,19 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import BeachesContainer from '../containers/BeachesContainer';
 
-class BeachesPage extends Component {
-  render() {
-    
-    return (
-      <>
-        <header className="App-header">
-          <h1>Your Saved Beaches:</h1>
-        </header>
+const BeachesPage = () => {    
+  return (
+    <>
+      <header className="App-header">
+        <h1>Your Saved Beaches:</h1>
+      </header>
 
-        <BeachesContainer />
-      </>
-    );
-  }
+      <BeachesContainer />
+    </>
+  );
 }
 
 export default BeachesPage;
