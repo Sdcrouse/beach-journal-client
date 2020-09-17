58d939d35dbbb70c182bd4625939a3a6f34baf97
sdcro <sdcrouse91@gmail.com>
2020-09-16 17:10:34 -0700

    Create an attractionsReducer, and add it to the combined reducers

--- src/store.js
+++ src/store.js
@@ -1,8 +1,12 @@
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import beachesReducer from './reducers/beachesReducer';
+import attractionsReducer from './reducers/attractionsReducer';
 
-const reducer = combineReducers({beaches: beachesReducer});
+const reducer = combineReducers({
+  beaches: beachesReducer,
+  attractions: attractionsReducer,
+});
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
