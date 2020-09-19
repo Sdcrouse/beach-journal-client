202a4bf9e5987c051dcf1b39ca108dcc1e3a043e
sdcro <sdcrouse91@gmail.com>
2020-09-18 19:05:14 -0700

    Display each beach's journal entries in the JournalEntriesContainer

--- src/containers/JournalEntriesContainer.js
+++ src/containers/JournalEntriesContainer.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class JournalEntriesContainer extends Component {
   render() {
+    const entriesByBeach = Object.values(this.props.journalEntries).filter(
+      entry => entry.beach_id === this.props.beachId
+    );
+
     return (
-      <p>Displaying Journal Entries for Beach with id {this.props.beachId}</p>
+      <>
+        <p>Displaying Journal Entries for Beach with id {this.props.beachId}:</p>
+        <ul>
+          {entriesByBeach.map( entry => <li key={entry.id}>{entry.title}</li> )}
+        </ul>
+      </>
     )
   }
 }
 
-export default JournalEntriesContainer;
+const mapStateToProps = ({ journalEntries }) => ({ journalEntries });
+
+export default connect(mapStateToProps)(JournalEntriesContainer);
