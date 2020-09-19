2148afeeb4862a18e93651531f3dba03e80c7e10
sdcro <sdcrouse91@gmail.com>
2020-09-19 01:23:25 -0700

    Move Switch, Route, and Link functionality from BeachCard to BeachesContainer
    As it turns out, it is possible to use Routes, Switch, etc. without using a React hook.
    It also looks better this and smoother this way:
    By clicking the 'View Beach' button, you now go from viewing the BeachCard to viewing the Beach, and the button disappears.
    Previously, clicking the 'View Beach' button would render the Beach AND the BeachCard.

--- src/components/BeachCard.js
+++ src/components/BeachCard.js
@@ -1,20 +1,10 @@
 import React from 'react';
-import Beach from '../components/Beach';
-import { Switch, Route, Link, useRouteMatch } from 'react-router-dom';
 
-const BeachCard = ({locationInfo, attractions, beachInfo}) => {
-  let match = useRouteMatch();
-
+const BeachCard = ({ beachInfo }) => {
   return (
     <>
       <h2>{beachInfo.name}</h2>
-      <Link to={`${match.url}/${beachInfo.id}`}><button>View Beach</button></Link>
-
-      <Switch>
-        <Route path={`${match.path}/${beachInfo.id}`}>
-          <Beach locationInfo={locationInfo} attractions={attractions} {...beachInfo} />
-        </Route>
-      </Switch>      
+      <p>{beachInfo.description}</p>   
     </>
   )
 };
--- src/containers/BeachesContainer.js
+++ src/containers/BeachesContainer.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import BeachCard from '../components/BeachCard';
+import Beach from '../components/Beach';
+import { Switch, Route, Link } from 'react-router-dom';
 
 class BeachesContainer extends Component {
   render() {
@@ -11,7 +13,17 @@ class BeachesContainer extends Component {
       const beachAttractions = Object.values(attractions).filter(attr => attr.beach_id === beach.id);
 
       return (
-        <BeachCard key={beach.id} locationInfo={location} attractions={beachAttractions} beachInfo={beach} />
+        <>
+          <Switch>
+            <Route path={`/beaches/${beach.id}`}>
+              <Beach locationInfo={location} attractions={beachAttractions} {...beach} />
+            </Route>
+            <Route path={'/beaches'}>
+              <BeachCard key={beach.id} beachInfo={beach} />
+              <Link to={`/beaches/${beach.id}`}><button>View Beach</button></Link>
+            </Route>
+          </Switch>
+        </>
       );
     });
 
