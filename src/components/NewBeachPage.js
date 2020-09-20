bcecb2c8f50d0a23cc8e36821346731617ce5872
sdcro <sdcrouse91@gmail.com>
2020-09-20 16:58:53 -0700

    Create form that outputs message to console when submitted

--- src/components/NewBeachPage.js
+++ src/components/NewBeachPage.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
 
 class NewBeachPage extends Component {
+  handleSubmit = event => {
+    event.preventDefault();
+
+    console.log("Beach form submitted!");
+  }
+
   render() {
     return (
-      <h1>New Beach</h1>
+      <>
+        <h1>New Beach</h1>
+
+        <form onSubmit={this.handleSubmit}>
+          <p>
+            <input type="submit" value="Create Beach" />
+          </p>
+        </form>
+      </>
     );
   }
 }
