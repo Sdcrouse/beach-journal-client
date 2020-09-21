f954d5dcb3a8594869700d15db2f95af1bc5964f
sdcro <sdcrouse91@gmail.com>
2020-09-20 17:00:58 -0700

    Add Beach inputs to New Beach form

--- src/components/NewBeachPage.js
+++ src/components/NewBeachPage.js
@@ -14,6 +14,22 @@ class NewBeachPage extends Component {
 
         <form onSubmit={this.handleSubmit}>
           <p>
+            <label htmlFor="name">Name: </label>
+            <input type="text" name="name" id="name" required />
+          </p>
+          <p>
+            <label htmlFor="description">Description: </label>
+            <textarea name="description" id="description" required />
+          </p>
+          <p>
+            <label htmlFor="items_to_bring">What to bring, wear, etc. </label>
+            <textarea name="items_to_bring" id="items_to_bring" />
+          </p>
+          <p>
+            <label htmlFor="popular_activities">Popular Activities: </label>
+            <textarea name="popular_activities" id="popular_activities" />
+          </p>
+          <p>
             <input type="submit" value="Create Beach" />
           </p>
         </form>
