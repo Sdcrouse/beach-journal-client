476cc74b8afb5c6b7ddddd3fc10af02501c6bdc2
sdcro <sdcrouse91@gmail.com>
2020-09-20 18:56:13 -0700

    Change NewBeachPage into a controlled component

--- src/components/NewBeachPage.js
+++ src/components/NewBeachPage.js
@@ -1,6 +1,19 @@
 import React, { Component } from 'react';
 
 class NewBeachPage extends Component {
+  state = {
+    name: '',
+    description: '',
+    items_to_bring: '',
+    popular_activities: ''
+  }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
   handleSubmit = event => {
     event.preventDefault();
 
@@ -15,19 +28,42 @@ class NewBeachPage extends Component {
         <form onSubmit={this.handleSubmit}>
           <p>
             <label htmlFor="name">Name: </label>
-            <input type="text" name="name" id="name" required />
+            <input
+              type="text"
+              name="name"
+              id="name"
+              value={this.state.name}
+              onChange={this.handleChange}
+              required
+            />
           </p>
           <p>
             <label htmlFor="description">Description: </label>
-            <textarea name="description" id="description" required />
+            <textarea
+              name="description"
+              id="description"
+              value={this.state.description}
+              onChange={this.handleChange}
+              required
+            />
           </p>
           <p>
             <label htmlFor="items_to_bring">What to bring, wear, etc. </label>
-            <textarea name="items_to_bring" id="items_to_bring" />
+            <textarea
+              name="items_to_bring"
+              id="items_to_bring"
+              value={this.state.items_to_bring}
+              onChange={this.handleChange}
+            />
           </p>
           <p>
             <label htmlFor="popular_activities">Popular Activities: </label>
-            <textarea name="popular_activities" id="popular_activities" />
+            <textarea
+              name="popular_activities"
+              id="popular_activities"
+              value={this.state.popular_activities}
+              onChange={this.handleChange}
+            />
           </p>
           <p>
             <input type="submit" value="Create Beach" />
