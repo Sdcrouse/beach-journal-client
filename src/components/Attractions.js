86c009d647ff24523eabe57d1f14ce7da3c2758a
sdcro <sdcrouse91@gmail.com>
2020-09-28 19:38:57 -0700

    Sort and display a beach's attractions by category

--- src/components/Attractions.js
+++ src/components/Attractions.js
@@ -1,14 +1,44 @@
 import React from 'react';
 
-const Attractions = ({ attractions }) => (
-  <>
-    <h2>Attractions:</h2>
-    {attractions.map(attraction => 
-      <p key={attraction.id}>
-        <strong>{attraction.name}: </strong>{attraction.description}
-      </p>
-    )}
-  </>
-);
+const Attractions = ({ attractions }) => {
+  const attrsByCategory = sortedByCategory(attractions);
+
+  return (
+    <>
+      <h2>Attractions:</h2>
+      <ul>
+        {Object.keys(attrsByCategory).map(category =>
+          <li key={category}>
+            <h3>{category}</h3>
+            <ul>
+              {attrsByCategory[category].map (attraction =>
+                <li key={attraction.id}>
+                  <strong>{attraction.name}: </strong>{attraction.description}
+                </li>
+              )}
+            </ul>
+          </li>
+        )}
+      </ul>
+    </>
+  );
+}
+
+const sortedByCategory = attractions => {
+  // Stretch goal: Sort these alphabetically. And is there a more efficient way to do all this?
+  // Right now, this takes O(n) time for each beach.
+
+  let sorted = {};
+
+  Object.values(attractions).forEach(attraction => {
+    if (sorted[attraction.category]) {
+      sorted[attraction.category].push(attraction);
+    } else {
+      sorted[attraction.category] = [attraction];
+    }
+  });
+
+  return sorted;
+};
 
 export default Attractions;
