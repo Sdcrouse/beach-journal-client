c0e79f7f337901b9bbf3b35ce5e116c1a2eb0ed0
sdcro <sdcrouse91@gmail.com>
2020-09-30 17:57:30 -0700

    Refactor JournalEntriesContainer with ES6 syntax

--- src/containers/JournalEntriesContainer.js
+++ src/containers/JournalEntriesContainer.js
@@ -6,8 +6,10 @@ import { Switch, Route, Link } from 'react-router-dom';
 
 class JournalEntriesContainer extends Component {
   render() {
-    const entriesByBeach = Object.values(this.props.journalEntries).filter(
-      entry => entry.beach_id === this.props.beachId
+    const { journalEntries, beachId, location } = this.props;
+
+    const entriesByBeach = Object.values(journalEntries).filter(
+      entry => entry.beach_id === beachId
     );
 
     let pageContent;
@@ -21,15 +23,15 @@ class JournalEntriesContainer extends Component {
     return (
       <>
         <h2>Journal Entries:</h2>
-        
-        {this.props.location.pathname === `/beaches/${this.props.beachId}` &&
-          <Link to={`/beaches/${this.props.beachId}/journal_entries/new`}>
+
+        {location.pathname === `/beaches/${beachId}` &&
+          <Link to={`/beaches/${beachId}/journal_entries/new`}>
             <button>New Journal Entry</button>
           </Link>
         }
 
         <Switch>
-          <Route path={`/beaches/${this.props.beachId}/journal_entries/new`}>
+          <Route path={`/beaches/${beachId}/journal_entries/new`}>
             <h3>New Journal Entry</h3>
           </Route>
         </Switch>
