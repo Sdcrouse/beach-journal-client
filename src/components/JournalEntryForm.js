08af036684a1c98f621e9dcea818efed581ba5cb
sdcro <sdcrouse91@gmail.com>
2020-09-30 19:38:32 -0700

    Change JournalEntryForm inputs into controlled components

--- src/components/JournalEntryForm.js
+++ src/components/JournalEntryForm.js
@@ -2,30 +2,47 @@ import React, { Component } from 'react';
 
 class JournalEntryForm extends Component {
   state = {
-    beachId: this.props.beachId
+    journalEntry: {
+      beachId: this.props.beachId,
+      date: '',
+      title: '',
+      topics: '',
+      entry_text: ''
+    }
   };
+
+  handleChange = event => {
+    this.setState({
+      journalEntry: {
+        ...this.state.journalEntry,
+        [event.target.name]: event.target.value
+      }
+    })
+  }
   
   render() {
+    const { date, title, topics, entry_text } = this.state.journalEntry;
+
     return (
       <>
         <h3>New Journal Entry</h3>
-        
+
         <form>
           <p>
             <label htmlFor="date">Date: </label>
-            <input type="text" id="date" name="date" />
+            <input type="text" id="date" name="date" value={date} onChange={this.handleChange} />
           </p>
           <p>
             <label htmlFor="title">Title: </label>
-            <input type="text" id="title" name="title" />
+            <input type="text" id="title" name="title" value={title} onChange={this.handleChange} />
           </p>
           <p>
             <label htmlFor="topics">Topics: </label>
-            <input type="text" id="topics" name="topics" />
+            <input type="text" id="topics" name="topics" value={topics} onChange={this.handleChange} />
           </p>
           <p>
             <label htmlFor="entry_text">Entry Text: </label>
-            <textarea id="entry_text" name="entry_text" />
+            <textarea id="entry_text" name="entry_text" value={entry_text} onChange={this.handleChange} />
           </p>
           <p>
             <input type="submit" value="Write this Journal Entry!" />
