26215b463b880567eb7de2d29b6d94db8f8dd98d
sdcro <sdcrouse91@gmail.com>
2020-10-01 00:07:27 -0700

    Send a POST fetch request to Rails backend with JournalEntry data

--- src/actions/journalEntryActions.js
+++ src/actions/journalEntryActions.js
@@ -1,5 +1,22 @@
+const BASE_URL = "http://localhost:3000/api/v1/beaches";
+
 export const createJournalEntry = entryData => {
+  const configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({ journal_entry: entryData.journalEntry })
+  };
+
+  const beachId = entryData.journalEntry.beach_id;
+
   return dispatch => {
-    console.log("Journal Entry saved! Here it is: ", entryData);
+    fetch(`${BASE_URL}/${beachId}/journal_entries`, configObj)
+      .then(response => response.json())
+      .then(journalJson => {
+        console.log("Journal Entry saved! Here it is: ", journalJson);
+      })
   }
 };
