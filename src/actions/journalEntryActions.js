9c2d3cea64e81fdf80c7a5d026b0d89ce7678f57
sdcro <sdcrouse91@gmail.com>
2020-10-01 00:12:59 -0700

    Minor edit: Refactor action with slightly DRY-er code

--- src/actions/journalEntryActions.js
+++ src/actions/journalEntryActions.js
@@ -1,19 +1,19 @@
 const BASE_URL = "http://localhost:3000/api/v1/beaches";
 
 export const createJournalEntry = entryData => {
+  const journal_entry = entryData.journalEntry;
+
   const configObj = {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
       "Accept": "application/json"
     },
-    body: JSON.stringify({ journal_entry: entryData.journalEntry })
+    body: JSON.stringify({ journal_entry })
   };
 
-  const beachId = entryData.journalEntry.beach_id;
-
   return dispatch => {
-    fetch(`${BASE_URL}/${beachId}/journal_entries`, configObj)
+    fetch(`${BASE_URL}/${journal_entry.beach_id}/journal_entries`, configObj)
       .then(response => response.json())
       .then(journalJson => {
         console.log("Journal Entry saved! Here it is: ", journalJson);
