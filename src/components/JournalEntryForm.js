d88c7e7a902dcae9b7d4638d9fc70cb70890b306
sdcro <sdcrouse91@gmail.com>
2020-09-30 19:24:50 -0700

    Create a form and input fields for a new Journal Entry

--- src/components/JournalEntryForm.js
+++ src/components/JournalEntryForm.js
@@ -6,7 +6,33 @@ class JournalEntryForm extends Component {
   };
   
   render() {
-    return <h3>New Journal Entry</h3>
+    return (
+      <>
+        <h3>New Journal Entry</h3>
+        
+        <form>
+          <p>
+            <label htmlFor="date">Date: </label>
+            <input type="text" id="date" name="date" />
+          </p>
+          <p>
+            <label htmlFor="title">Title: </label>
+            <input type="text" id="title" name="title" />
+          </p>
+          <p>
+            <label htmlFor="topics">Topics: </label>
+            <input type="text" id="topics" name="topics" />
+          </p>
+          <p>
+            <label htmlFor="entry_text">Entry Text: </label>
+            <textarea id="entry_text" name="entry_text" />
+          </p>
+          <p>
+            <input type="submit" value="Write this Journal Entry!" />
+          </p>
+        </form>
+      </>
+    )
   }
 }
 
