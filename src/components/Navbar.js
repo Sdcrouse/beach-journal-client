6a785b94ba09a758234fe98eef7c4bfdd6bfb2c8
sdcro <sdcrouse91@gmail.com>
2020-10-02 23:23:53 -0700

    Use React-Bootstrap and CSS to change spacing and colors of Navlinks

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,39 +1,51 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const linkStyles = {
-  width: '100px',
-  padding: '12px',
-  margin: '0 6px 6px',
-  background: 'blue',
+  padding: '5%',
+  background: '#BCE5F9',
   textDecoration: 'none',
-  color: 'white'
+  border: '1px solid white',
+  color: 'black'
 };
 
-const activeStyles = { background: 'darkblue' };
+const activeStyles = { background: '#00B3F8', color: 'white' };
 
 const Navbar = () => (
   <nav>
-    <NavLink
-      exact
-      to="/"
-      style={linkStyles}
-      activeStyle={activeStyles}
-    >Home</NavLink>
-    
-    <NavLink
-      exact
-      to="/beaches"
-      style={linkStyles}
-      activeStyle={activeStyles}
-    >Your Saved Beaches</NavLink>
+    <Container>
+      <Row>
+        <Col>
+          <NavLink
+            exact
+            to="/"
+            style={linkStyles}
+            activeStyle={activeStyles}
+          >Home</NavLink>        
+        </Col>
+        
+        <Col>
+          <NavLink
+            exact
+            to="/beaches"
+            style={linkStyles}
+            activeStyle={activeStyles}
+          >Your Saved Beaches</NavLink>
+        </Col>
 
-    <NavLink
-      exact
-      to="/beaches/new"
-      style={linkStyles}
-      activeStyle={activeStyles}
-    >New Beach</NavLink>
+        <Col>
+          <NavLink
+            exact
+            to="/beaches/new"
+            style={linkStyles}
+            activeStyle={activeStyles}
+          >New Beach</NavLink>
+        </Col>
+      </Row>
+    </Container>
   </nav>
 );
 
