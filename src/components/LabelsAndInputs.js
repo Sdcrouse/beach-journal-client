917501c8d2bf54382b99b34f603151504d3f7c6d
sdcro <sdcrouse91@gmail.com>
2020-10-05 00:44:23 -0700

    Add new prop to LabeledInput, and give its controlId a default value
    The idea here is flexibility: By default, the FormGroup's controlId value is set to the inputValue (the 'name' of the <input>).
    But if an inputId is specified (i.e. an 'id' that is different from the <input>'s 'name'),
    the controlId's value is set to that instead.

--- src/components/LabelsAndInputs.js
+++ src/components/LabelsAndInputs.js
@@ -4,19 +4,24 @@ import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
-export const LabeledInput = ({ inputName, inputValue, labelClass, labelText, onChange, required }) => 
-  <Form.Group as={Row} controlId={inputName} className="center-justify-content">
-    <Form.Label className={labelClass}>{labelText}</Form.Label>
-    <Col xs="auto">
-      <Form.Control
-        type="text"
-        name={inputName}
-        value={inputValue}
-        onChange={onChange}
-        required={required}
-      />
-    </Col>
-  </Form.Group>
+export const LabeledInput = ({ inputName, inputValue, inputId, labelClass, labelText, onChange, required }) => {
+  const controlId = inputId ? inputId : inputName;
+  
+  return (
+    <Form.Group as={Row} controlId={controlId} className="center-justify-content">
+      <Form.Label className={labelClass}>{labelText}</Form.Label>
+      <Col xs="auto">
+        <Form.Control
+          type="text"
+          name={inputName}
+          value={inputValue}
+          onChange={onChange}
+          required={required}
+        />
+      </Col>
+    </Form.Group>
+  );
+};
 
 export const LabeledTextarea = ({ inputName, inputValue, labelClass, labelText, onChange, required, colSize }) =>
   <Form.Group as={Row} controlId={inputName} className="center-justify-content">
