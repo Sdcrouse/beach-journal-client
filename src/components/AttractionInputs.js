7d672830fc5e2d9edfa0ac16895b7f37503c376c
sdcro <sdcrouse91@gmail.com>
2020-10-05 18:12:44 -0700

    Add row/column structure to Attraction inputs

--- src/components/AttractionInputs.js
+++ src/components/AttractionInputs.js
@@ -1,43 +1,51 @@
 import React from 'react';
 import '../App.css';
 import { LabeledInput, LabeledTextarea } from './LabelsAndInputs';
+import Form from 'react-bootstrap/Form';
+import Col from 'react-bootstrap/Col';
 
 const AttractionInputs = ({ index, category, name, description, handleChange }) => {
   let categoryId = `category-${index}`, nameId = `name-${index}`, descId = `description-${index}`;
   return (
-    <>
-      <LabeledInput
-        inputName="category"
-        inputValue={category}
-        inputId={categoryId}
-        inputDataId={index}
-        labelClass="required-field"
-        labelText="Category:"
-        onChange={handleChange}
-        required={true}
-      />
-      <LabeledInput
-        inputName="name"
-        inputValue={name}
-        inputId={nameId}
-        inputDataId={index}
-        labelClass="required-field"
-        labelText="Name:"
-        onChange={handleChange}
-        required={true}
-      />
-      <LabeledTextarea
-        inputName="description"
-        inputValue={description}
-        inputId={descId}
-        inputDataId={index}
-        labelClass="required-field"
-        labelText="Description:"
-        onChange={handleChange}
-        required={true}
-        colSize={5}
-      />
-    </>
+    <Form.Row>
+      <Col xs={3}>
+        <LabeledInput
+          inputName="category"
+          inputValue={category}
+          inputId={categoryId}
+          inputDataId={index}
+          labelClass="required-field"
+          labelText="Category:"
+          onChange={handleChange}
+          required={true}
+        />
+      </Col>
+      <Col xs={3}>
+        <LabeledInput
+          inputName="name"
+          inputValue={name}
+          inputId={nameId}
+          inputDataId={index}
+          labelClass="required-field"
+          labelText="Name:"
+          onChange={handleChange}
+          required={true}
+        />
+      </Col>
+      <Col xs={6}>
+        <LabeledTextarea
+          inputName="description"
+          inputValue={description}
+          inputId={descId}
+          inputDataId={index}
+          labelClass="required-field"
+          labelText="Description:"
+          onChange={handleChange}
+          required={true}
+          colSize={10}
+        />
+      </Col>
+    </Form.Row>
   );
 };
 
