71ddbf11e178f69b0e47b7d46512b99dd8cf1f88
sdcro <sdcrouse91@gmail.com>
2020-10-07 21:37:14 -0700

    Display a Location's state unless its value is N/A

--- src/components/Location.js
+++ src/components/Location.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
-const Location = ({ city, state, country }) => (
-  <p>
-    <strong className="beach-info-labels">Location: </strong>{city}, {state}, {country}
-  </p>
-);
+const Location = ({ city, state, country }) => {
+  let locationInfo;
+
+  if (state === "N/A" || state === "n/a" || state === "N/a") {
+    locationInfo = <>{city}, {country}</>;
+  } else {
+    locationInfo = <>{city}, {state}, {country}</>;
+  }
+
+  return (
+    <p>
+      <strong className="beach-info-labels">Location: </strong>{locationInfo}
+    </p>
+  )
+};
 
 export default Location;
