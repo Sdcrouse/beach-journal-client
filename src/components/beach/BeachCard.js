0ece70aaab893c2ff867f21b0962723e2ae6d7cb
sdcro <sdcrouse91@gmail.com>
2020-10-15 09:28:05 -0700

    Add a button that adds 'likes' to beaches
    Note: This was done as a code challenge for my project assessment.
    I can add this in for real on a future version, but with hooks and styling.
    At the moment, it only changes the beach on the frontend, not the backend.

--- src/components/beach/BeachCard.js
+++ src/components/beach/BeachCard.js
@@ -1,25 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import Location from '../location/Location';
 import '../../App.css';
 
-const BeachCard = ({ beachInfo, locationInfo }) => {
-  return (
-    <>
-      <br />
-      <div className="beach-card">
-        <h2>{beachInfo.name}</h2>
-        
-        {locationInfo &&
-          <p><Location {...locationInfo} /></p>
-        }
-        
-        <p>{beachInfo.description}</p>
-        <Link to={`/beaches/${beachInfo.id}`}><button>View Beach</button></Link>
-      </div>
-      <br />
-    </>
-  )
+class BeachCard extends Component {
+  state = {
+    likes: 0
+  }
+
+  handleClick = event => {
+    event.preventDefault();
+
+    this.setState(currentState => {
+      return ({
+        likes: currentState.likes + 1
+      })
+    })
+  }
+
+  render () {
+    const { beachInfo, locationInfo } = this.props;
+    return (
+      <>
+        <br />
+        <div className="beach-card">
+          <h2>{beachInfo.name}</h2>
+          
+          {locationInfo &&
+            <p><Location {...locationInfo} /></p>
+          }
+          
+          <p>{beachInfo.description}</p>
+          <Link to={`/beaches/${beachInfo.id}`}><button>View Beach</button></Link>
+          <button onClick={this.handleClick}>Add a "like" to this Beach</button>
+          <p>{this.state.likes}</p>
+        </div>
+        <br />
+      </>
+    )
+  }
 };
 
 export default BeachCard;
