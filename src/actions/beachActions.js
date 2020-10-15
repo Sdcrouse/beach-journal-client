755f69a94e68d9acdfaec0648a7659dcd14edf7e
sdcro <sdcrouse91@gmail.com>
2020-10-14 17:05:38 -0700

    Add note to developers about how the normalization functions work

--- src/actions/beachActions.js
+++ src/actions/beachActions.js
@@ -123,3 +123,123 @@ const normalizeBeach = beachData => {
 
   return normalized;
 };
+
+/* Note to developers:
+
+   As of this version of the Beach Journal, normalizeBeaches() takes an array of objects that looks like this:
+   [
+     {
+      "id": "1",
+      "type": "beach",
+      "attributes": {
+        "name": "Beach A",
+        "description": "Lorem ipsum",
+        "items_to_bring": "Dolor sit amet",
+        "popular_activities": "Consectetur apidiscing amet",
+        "location": {
+          "id": 2,
+          "city": "City B",
+          "state": "State B",
+          "country": "Country B"
+        },
+        "attractions": [
+          {
+            "id": 1,
+            "beach_id": 1,
+            "category": "Category A",
+            "name": "Attraction A",
+            "description": "Description A"
+          },
+          {
+            "id": 2,
+            "beach_id": 1,
+            "category": "Category B",
+            "name": "Attraction B",
+            "description": "Description B"
+          }
+        ],
+        "journal_entries": [
+          {
+            "id": 1,
+            "beach_id": 1,
+            "title": "Title 1",
+            "topics: "A, B, C",
+            "entry_text": "Entry text A"
+          },
+          {
+            "id": 2,
+            "beach_id": 1,
+            "title": "Title 2",
+            "topics: "D, E, F",
+            "entry_text": "Entry text B"
+          }          
+        ]
+      }
+     },
+     {
+      "id": 2,
+      "type": "beach",
+      "attributes": {...}
+     }
+   ]
+
+   and converts it into this nested object:
+   {
+     beaches: {
+       1: {
+         id: 1,
+         location_id: 2,
+         name: Beach A",
+         description: "Lorem ipsum",
+         items_to_bring: "Dolor sit amet",
+         popular_activities: "Consectetur apidiscing amet"
+       },
+       2: {
+         id: 2,...
+       }
+     },
+     locations: {
+       2: {
+         "id": 2,
+         "city": "City B",
+         "state": "State B",
+         "country": "Country B"
+       },...
+     },
+     attractions: {
+       1: {
+         "id": 1,
+         "beach_id": 1,
+         "category": "Category A",
+         "name": "Attraction A",
+         "description": "Description A"
+       },
+       2: {
+         "id": 2,
+         "beach_id": 1,
+         "category": "Category B",
+         "name": "Attraction B",
+         "description": "Description B"
+       },...
+     },
+     journal_entries: {
+       1: {
+         "id": 1,
+         "beach_id": 1,
+         "title": "Title 1",
+         "topics: "A, B, C",
+         "entry_text": "Entry text A"
+       },
+       2: {
+         "id": 2,
+         "beach_id": 1,
+         "title": "Title 2",
+         "topics: "D, E, F",
+         "entry_text": "Entry text B"
+       },...
+     }
+   }
+
+   A similar data structure conversion occurs in normalizeBeach, after a beach is created.
+   The goal here is to make it easy to access and update data in the reducers.
+*/
