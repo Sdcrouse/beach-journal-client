e06b2a72eb685562cc445d95c4bb5eb74c61e88b
sdcro <sdcrouse91@gmail.com>
2020-10-26 14:59:17 -0700

    Convert BeachesContainer to functional component with useSelector hooks

--- src/containers/BeachesContainer.js
+++ src/containers/BeachesContainer.js
@@ -1,55 +1,50 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React from 'react';
+import { useSelector } from 'react-redux';
 import BeachCard from '../components/beach/BeachCard';
 import Beach from '../components/beach/Beach';
 import { Switch, Route } from 'react-router-dom';
 import '../App.css';
 import Container from 'react-bootstrap/Container';
 
-class BeachesContainer extends Component {
-  render() {
-    const { beaches, locations, attractions } = this.props;
-    let [beachRoutes, beachCards] = [ [], [] ];
+const BeachesContainer = props => {
+  const beaches = useSelector(state => state.beachData.beaches);
+  const locations = useSelector(state => state.locations);
+  const attractions = useSelector(state => state.attractions);
 
-    for (const beach of Object.values(beaches)) {
-      const location = locations[beach.location_id];
-      const beachAttractions = Object.values(attractions).filter(attr => attr.beach_id === beach.id);
+  let [beachRoutes, beachCards] = [ [], [] ];
 
-      beachRoutes.push(
-        <Route path={`/beaches/${beach.id}`} key={beach.id}>
-          <Beach locationInfo={location} attractions={beachAttractions} {...beach} />
-        </Route>
-      );
+  for (const beach of Object.values(beaches)) {
+    const location = locations[beach.location_id];
+    const beachAttractions = Object.values(attractions).filter(attr => attr.beach_id === beach.id);
 
-      beachCards.push(<BeachCard beachInfo={beach} locationInfo={location} key={beach.id} />);
-    }
-
-    return (
-      <Switch>
-        {beachRoutes}
-        <Route path={'/beaches'}>
-
-          {this.props.location.state && 
-            <h4 className="success-message">{this.props.location.state.successMessage}</h4>
-          }
-
-          <header className="App-header">
-            <h1>Your Saved Beaches:</h1>
-          </header>
-
-          <Container>
-            {beachCards}
-          </Container>
-        </Route>
-      </Switch>
+    beachRoutes.push(
+      <Route path={`/beaches/${beach.id}`} key={beach.id}>
+        <Beach locationInfo={location} attractions={beachAttractions} {...beach} />
+      </Route>
     );
+
+    beachCards.push(<BeachCard beachInfo={beach} locationInfo={location} key={beach.id} />);
   }
+
+  return (
+    <Switch>
+      {beachRoutes}
+      <Route path={'/beaches'}>
+
+        {props.location.state && 
+          <h4 className="success-message">{props.location.state.successMessage}</h4>
+        }
+
+        <header className="App-header">
+          <h1>Your Saved Beaches:</h1>
+        </header>
+
+        <Container>
+          {beachCards}
+        </Container>
+      </Route>
+    </Switch>
+  );
 }
 
-const mapStateToProps = state => ({ 
-  beaches: state.beachData.beaches,
-  locations: state.locations,
-  attractions: state.attractions
-});
-
-export default connect(mapStateToProps)(BeachesContainer);
+export default BeachesContainer;
