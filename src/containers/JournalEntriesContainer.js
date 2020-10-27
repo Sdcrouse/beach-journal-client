8456f030681131ff2ca9ebad2f1b36057041eef7
sdcro <sdcrouse91@gmail.com>
2020-10-26 17:27:47 -0700

    Convert JournalEntriesContainer to functional component with a useSelector hook

--- src/containers/JournalEntriesContainer.js
+++ src/containers/JournalEntriesContainer.js
@@ -1,55 +1,52 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React from 'react';
+import { useSelector } from 'react-redux';
 import JournalEntry from '../components/journalEntry/JournalEntry';
 import { withRouter } from 'react-router';
 import { Switch, Route, Link } from 'react-router-dom';
 import JournalEntryForm from '../components/journalEntry/JournalEntryForm';
 import Container from 'react-bootstrap/Container';
 
-class JournalEntriesContainer extends Component {
-  render() {
-    const { journalEntries, beachId, location } = this.props;
+const JournalEntriesContainer = props => {
+  const journalEntries = useSelector(state => state.journalEntries);
+  const { beachId, location } = props;
 
-    const entriesByBeach = Object.values(journalEntries).filter(
-      entry => entry.beach_id === beachId
-    );
+  const entriesByBeach = Object.values(journalEntries).filter(
+    entry => entry.beach_id === beachId
+  );
 
-    let pageContent;
+  let pageContent;
 
-    if (entriesByBeach.length > 0) {
-      pageContent = entriesByBeach.map( entry => <JournalEntry key={entry.id} {...entry} /> )
-    } else {
-      pageContent = <p>No journal entries yet. Feel free to write one!</p>
-    }
-
-    return (
-      <>
-        <h2 className="secondary-labels">Journal Entries:</h2>
-
-        {location.state && 
-          <h4 className="success-message">{location.state.successMessage}</h4>
-        }
-
-        {location.pathname === `/beaches/${beachId}` &&
-          <Link to={`/beaches/${beachId}/journal_entries/new`}>
-            <button>New Journal Entry</button>
-          </Link>
-        }
-
-        <Switch>
-          <Route path={`/beaches/${beachId}/journal_entries/new`}>
-            <JournalEntryForm beachId={beachId} />
-          </Route>
-        </Switch>
-
-        <Container>
-          {pageContent}
-        </Container>
-      </>
-    )
+  if (entriesByBeach.length > 0) {
+    pageContent = entriesByBeach.map( entry => <JournalEntry key={entry.id} {...entry} /> )
+  } else {
+    pageContent = <p>No journal entries yet. Feel free to write one!</p>
   }
+
+  return (
+    <>
+      <h2 className="secondary-labels">Journal Entries:</h2>
+
+      {location.state && 
+        <h4 className="success-message">{location.state.successMessage}</h4>
+      }
+
+      {location.pathname === `/beaches/${beachId}` &&
+        <Link to={`/beaches/${beachId}/journal_entries/new`}>
+          <button>New Journal Entry</button>
+        </Link>
+      }
+
+      <Switch>
+        <Route path={`/beaches/${beachId}/journal_entries/new`}>
+          <JournalEntryForm beachId={beachId} />
+        </Route>
+      </Switch>
+
+      <Container>
+        {pageContent}
+      </Container>
+    </>
+  )
 }
 
-const mapStateToProps = ({ journalEntries }) => ({ journalEntries });
-
-export default withRouter( connect(mapStateToProps)(JournalEntriesContainer) );
+export default withRouter(JournalEntriesContainer);
