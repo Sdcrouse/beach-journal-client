c732882cc7c1711ec30660f8bc25ecabcfa6d4af
sdcro <sdcrouse91@gmail.com>
2020-10-27 17:41:17 -0700

    Performance optimization: Refactor BeachesContainer with memoized selector
    The Reselect package allows you to memoize your selectors, to prevent unnecessary re-rendering when part of the state changes.
    createStructuredSelector simplifies this, allowing you to return an object with multiple pieces of state similarly to mapStateToProps.

--- src/components/beach/BeachesContainer.js
+++ src/components/beach/BeachesContainer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { createStructuredSelector } from 'reselect';
 import { useSelector } from 'react-redux';
 import BeachCard from './BeachCard';
 import Beach from './Beach';
@@ -6,10 +7,18 @@ import { Switch, Route } from 'react-router-dom';
 import '../../App.css';
 import Container from 'react-bootstrap/Container';
 
+const beachesSelector = state => state.beachData.beaches;
+const locationsSelector = state => state.locations;
+const attractionsSelector = state => state.attractions;
+
+const structuredBeachesSelector = createStructuredSelector({
+  beaches: beachesSelector,
+  locations: locationsSelector,
+  attractions: attractionsSelector
+});
+
 const BeachesContainer = props => {
-  const beaches = useSelector(state => state.beachData.beaches);
-  const locations = useSelector(state => state.locations);
-  const attractions = useSelector(state => state.attractions);
+  const { beaches, locations, attractions } = useSelector(structuredBeachesSelector);
 
   let [beachRoutes, beachCards] = [ [], [] ];
 
