92d9efe3b96c66755c430ca07328c1294b429128
sdcro <sdcrouse91@gmail.com>
2020-10-27 17:35:21 -0700

    Fix broken FigureImage src
    The 'src' attribute broke when I upgraded to the latest NPM version.
    This may have caused other breaking changes that I haven't caught yet.

--- src/App.js
+++ src/App.js
@@ -48,7 +48,7 @@ class App extends Component {
     return (
       <div className="App">
         <header>
-          <FigureImage alt="Beach ball and Beach Journal title" src={require('./beach-journal-header.png')} width="100%" />
+          <FigureImage alt="Beach ball and Beach Journal title" src={require('./beach-journal-header.png').default} width="100%" />
           <FigureCaption className="custom-caption">
             Original beach ball image by <a href="https://pixabay.com/users/zrblue-10475804/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=5422214">ZRblue</a> from <a href="https://pixabay.com/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=5422214">Pixabay</a>
           </FigureCaption>
