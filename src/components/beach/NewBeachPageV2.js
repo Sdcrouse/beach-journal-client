b45b836b8ca4fb0b7e7bc953436ea79309bbf8d5
sdcro <sdcrouse91@gmail.com>
2020-11-04 23:53:26 -0800

    Add a simple React-Bootstrap form that outputs a message when submitted

--- src/components/beach/NewBeachPageV2.js
+++ src/components/beach/NewBeachPageV2.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import '../../App.css';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 const newBeachPageV2 = () => {
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    console.log("Beach form submitted!");
+  }
+
   return (
     <>
       <h1>New Beach</h1>
       <p><strong>* </strong><span className="required-field">Required field</span></p>
+
+      <Form onSubmit={handleSubmit}>
+        <Button type="submit">Create Beach</Button>
+      </Form>
     </>
   );
 }
