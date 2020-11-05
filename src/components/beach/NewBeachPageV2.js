57fc91f11ff6347c3876eff2ca7756ac47823977
sdcro <sdcrouse91@gmail.com>
2020-11-04 23:44:43 -0800

    Create the basic structure of a new NewBeachPage component
    The idea with these next few commits is to ultimately change the NewBeachPage into a functional component with hooks.

---
+++ src/components/beach/NewBeachPageV2.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../../App.css';
+
+const newBeachPageV2 = () => {
+  return (
+    <>
+      <h1>New Beach</h1>
+      <p><strong>* </strong><span className="required-field">Required field</span></p>
+    </>
+  );
+}
+
+export default newBeachPageV2;
