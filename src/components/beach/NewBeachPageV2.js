83bdf16b08d341200005d224e06ee30dbe6a0117
sdcro <sdcrouse91@gmail.com>
2020-11-05 17:28:03 -0800

    Add label and controlled input field for a beach's name

--- src/components/beach/NewBeachPageV2.js
+++ src/components/beach/NewBeachPageV2.js
@@ -2,14 +2,26 @@ import React, { useState } from 'react';
 import LocationInputs from '../location/LocationInputs';
 import '../../App.css';
 import Form from 'react-bootstrap/Form';
+import { LabeledInput } from '../LabelsAndInputs';
 import Button from 'react-bootstrap/Button';
 
 const NewBeachPageV2 = () => {
+  const [beachData, setBeachData] = useState({
+    name: ''
+  });
+
   const [location, setLocation] = useState({
     city: '',
     state: '',
     country: ''
   });
+
+  const handleChange = event => {
+    setBeachData({
+      ...beachData,
+      [event.target.name]: event.target.value
+    })
+  }
 
   const handleLocationInputChange = event => {
     setLocation({
@@ -21,6 +33,7 @@ const NewBeachPageV2 = () => {
   const handleSubmit = event => {
     event.preventDefault();
     console.log("Beach form submitted!");
+    console.log("Beach data: ", beachData);
     console.log("Location Data: ", location);
   }
 
@@ -30,6 +43,15 @@ const NewBeachPageV2 = () => {
       <p><strong>* </strong><span className="required-field">Required field</span></p>
 
       <Form onSubmit={handleSubmit}>
+        <LabeledInput
+          inputName="name"
+          inputValue={beachData.name}
+          labelClass="required-field"
+          labelText="Name:"
+          onChange={handleChange}
+          required={true}
+        />
+
         <LocationInputs handleChange={handleLocationInputChange} {...location} />
 
         <Button type="submit">Create Beach</Button>
