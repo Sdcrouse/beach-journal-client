a7865a44faaeaf762da283c6242fc61dd70c4bee
sdcro <sdcrouse91@gmail.com>
2020-11-05 17:02:44 -0800

    Add Location inputs to NewBeachPageV2 form

--- src/components/beach/NewBeachPageV2.js
+++ src/components/beach/NewBeachPageV2.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import LocationInputs from '../location/LocationInputs';
 import '../../App.css';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
 const NewBeachPageV2 = () => {
+  const [location, setLocation] = useState({
+    city: '',
+    state: '',
+    country: ''
+  });
+
+  const handleLocationInputChange = event => {
+    setLocation({
+      ...location,
+      [event.target.name]: event.target.value
+    })
+  }
 
   const handleSubmit = event => {
     event.preventDefault();
     console.log("Beach form submitted!");
+    console.log("Location Data: ", location);
   }
 
   return (
@@ -16,6 +30,8 @@ const NewBeachPageV2 = () => {
       <p><strong>* </strong><span className="required-field">Required field</span></p>
 
       <Form onSubmit={handleSubmit}>
+        <LocationInputs handleChange={handleLocationInputChange} {...location} />
+
         <Button type="submit">Create Beach</Button>
       </Form>
     </>
